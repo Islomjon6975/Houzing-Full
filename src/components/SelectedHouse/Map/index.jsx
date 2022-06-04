fa099d89bfb2ec72a0adde0d04e4960c040908b2
islomjon6975 <islomjon0919@mail.ru>
2022-06-04 11:22:24 +0500

    map is being added

--- src/components/SelectedHouse/Map/index.jsx
+++ src/components/SelectedHouse/Map/index.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 import { Container, Description, Subtitle, Title, Wrapper } from './style'
 
 export const Map = () => {
+
+    const containerStyle = {
+        width: '100%',
+        height: '450px'
+    };
+    
+    const center = {
+        lat: 41.311081,
+        lng: 69.240562
+    };
+    
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "AIzaSyAkkKvMyf8Tk3Q8s7MWXin6njbtjIjq2S4"
+    })
+    
+    const [map, setMap] = React.useState(null)
+    
+    const onLoad = React.useCallback(function callback(map) {
+        const bounds = new window.google.maps.LatLngBounds(center);
+        map.fitBounds(bounds);
+        setMap(map)
+    }, [])
+    
+    const onUnmount = React.useCallback(function callback(map) {
+        setMap(null)
+    }, [])
+
   return (
     <Container>
         <Title>Location</Title>
@@ -19,7 +48,7 @@ export const Map = () => {
                 <Description>Greenville</Description>
             </Wrapper>
         </Wrapper>
-        <Wrapper mt={24}>
+        <Wrapper mt={24} mb={24}>
             <Wrapper>
                 <Subtitle>State/County: </Subtitle>
                 <Description>Washington</Description>
@@ -33,6 +62,20 @@ export const Map = () => {
                 <Description>United States</Description>
             </Wrapper>
         </Wrapper>
+        {
+            isLoaded ? (
+                <GoogleMap
+                  mapContainerStyle={containerStyle}
+                  center={center}
+                  zoom={10}
+                  onLoad={onLoad}
+                  onUnmount={onUnmount}
+                >
+                  { /* Child components, such as markers, info windows, etc. */ }
+                  <></>
+                </GoogleMap>
+            ) : <></>
+        }
     </Container>
   )
 }
